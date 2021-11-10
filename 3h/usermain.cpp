feb12014eba4760b42806c1633749453cb7c850f
hcc1 <2280057905@qq.com>
2021-11-10 16:06:08 +0800

    更新3号机，新增低水位报警功能，记得数据条目和数据最大小值的数据类型要保持相同

--- 3h/usermain.cpp
+++ 3h/usermain.cpp
@@ -1,27 +1,33 @@
 #include "usermain.h"
 extern "C"
 {
-	const char *MODE_INFO = "@断电记忆[0-2]:关闭，仅本次，所有@水位[0-1]:正常，过低";
+	const char *MODE_INFO = "@断电记忆[0-2]:关闭，仅本次，所有@水位[0-1]:正常，过低@水泵模式[0-1]:手动，定时，禁用";
+	struct Udpwarn shuiwei_low_error = {WARN, NOT_WARN, 0, 1, "水位过低，请及时补水"};
 
 	uint8_t power_save = 0; //断电记忆
 
 	//两个开关，当他为2时，是自动模式，其他时候读取12 和14号脚的电平
 	double guang_qiang = 0; //光照强度
 	uint8_t shui_wei = 0;
-	uint8_t LED2 = 0;
+	uint8_t shui_beng = 0;
+	uint8_t shui_beng_mode = 0;
+	uint8_t s_b_time1 = 0;
 	uint8_t yu_men[4 * 2 + 3 * 2] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //与门寄存器
 
 	//下面定义几个引脚的功能
-	const uint8_t huo_er = 14; //1号继电器
-	const uint8_t jd2 = 12;	   //2号继电器
-	const uint8_t anjian1 = 0; //按键1输入
+	const uint8_t p_shuiwei = 14; //水位
+	const uint8_t jd2 = 12;		  //继电器
+	const uint8_t anjian1 = 0;	  //按键1输入
 
-	unsigned char CONST1[3] = {0, 1, 2};
+	//最大值最小值务必和数据项目的数据类型保持一致
+	unsigned char CONST1[4] = {0, 1, 2, 100};
 
 	struct MyType data_list[MAX_NAME] = {
 		{"光强", "%", TYPE_DOUBLE, sizeof(guang_qiang), &guang_qiang, NULL, NULL},
 		{"水位", NULL, TYPE_u8, sizeof(shui_wei), &shui_wei, CONST1, CONST1 + 1},
-		{"@水泵", NULL, TYPE_u8, sizeof(LED2), &LED2, CONST1, CONST1 + 1},
+		{"@水泵模式", NULL, TYPE_u8, sizeof(shui_beng_mode), &shui_beng_mode, CONST1, CONST1 + 2},
+		{"@水泵", NULL, TYPE_u8, sizeof(shui_beng), &shui_beng, CONST1, CONST1 + 1},
+		{"@水泵自动关闭计时", "S", TYPE_u8, sizeof(s_b_time1), &s_b_time1, CONST1, CONST1 + 3},
 		{"@断电记忆", NULL, TYPE_u8, sizeof(power_save), &power_save, CONST1, CONST1 + 2},
 		{"@1与1入", NULL, TYPE_u8, sizeof(yu_men[0]), yu_men, CONST1, CONST1 + 1},		//1号与门1号入口
 		{"@1与2入", NULL, TYPE_u8, sizeof(yu_men[0]), yu_men + 1, CONST1, CONST1 + 1},	//1号与门2号入口
@@ -43,9 +49,28 @@ extern "C"
 
 	void timer1_worker()
 	{
+		static uint8 shui_wei_count = 0;
 		clear_wifi_data(wifi_ssid_pw_file); //长按按键1清除wifi账号密码记录
-		shui_wei = digitalRead(huo_er);
+		shui_wei = digitalRead(p_shuiwei);
 		guang_qiang = system_adc_read() * 100.00 / 1024;
+
+		if (shui_wei == 1)
+		{
+			shui_wei_count = shui_wei_count + 1;
+			if (shui_wei_count > 25) //连续多次检测到低水位
+			{
+				if (shuiwei_low_error.status == NOT_WARN)
+				{
+					set_warn(&shuiwei_low_error);
+				}
+				shui_wei_count = 25;
+			}
+		}
+		else
+		{
+			shui_wei_count = 0;
+			shuiwei_low_error.status = NOT_WARN;
+		}
 	}
 
 	void my_init()
@@ -61,12 +86,27 @@ extern "C"
 	void add_values()
 	{
 		//add_value(&LED1, sizeof(LED1));
-		add_value(&LED2, sizeof(LED2));
+		add_value(&shui_beng, sizeof(shui_beng));
+		add_value(&shui_beng_mode, sizeof(shui_beng_mode));
 	}
 
 	//每隔 1s
 	void ruan_timer_1s()
 	{
+		if (shui_beng_mode == 1)
+		{
+			if (s_b_time1 > 0)
+			{
+				shui_beng = 1;
+				digitalWrite(jd2, LOW);
+				s_b_time1 = s_b_time1 - 1;
+			}
+			else
+			{
+				shui_beng = 1;
+				digitalWrite(jd2, HIGH);
+			}
+		}
 	}
 
 	//每隔 RUAN_TIMEer_ms
@@ -90,16 +130,38 @@ extern "C"
 		yu_men[10] = yu_men[8] & yu_men[9];
 		yu_men[13] = yu_men[11] & yu_men[12];
 
-		shui_wei = digitalRead(huo_er);
+		//不要在这里采集数据，模拟信号有很大的误差
+		//shui_wei = digitalRead(p_shuiwei);
+		//guang_qiang = system_adc_read() * 100 / 1024.00;
 
-		guang_qiang = system_adc_read() * 100 / 1024.00;
-
-		if (LED2 == 1)
-		{
-			digitalWrite(jd2, LOW);
+		if (shui_beng_mode == 0)
+		{ //手动模式
+			if (shui_beng == 1)
+			{
+				digitalWrite(jd2, LOW);
+			}
+			else
+			{
+				digitalWrite(jd2, HIGH);
+			}
+			s_b_time1 = 0;
+		}
+		else if (shui_beng_mode == 1)
+		{ //定时模式
+			if (s_b_time1 > 0)
+			{
+				shui_beng = 1;
+				digitalWrite(jd2, LOW);
+			}
+			else
+			{
+				shui_beng = 0;
+				digitalWrite(jd2, HIGH);
+			}
 		}
 		else
-		{
+		{ //禁用
+			shui_beng = 0;
 			digitalWrite(jd2, HIGH);
 		}
 	}
--- 3h/usermain.h
+++ 3h/usermain.h
@@ -36,7 +36,7 @@ extern "C"
 	void refresh_work();
 		
 	//str_data_names 的长度
-	#define MAX_NAME 20
+	#define MAX_NAME 25
 	//extern const char *str_data_names[MAX_NAME];
 	extern struct MyType data_list[MAX_NAME];
 	//可选变量描述
